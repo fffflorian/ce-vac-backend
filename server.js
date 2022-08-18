7e0133ea27ef60666873e404b8a6df3de94539a8
Florian Frank <florian.ff.frank@fau.de>
2022-08-18 12:57:14 +0200

    Fixes Site

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
 	credentials: true,
 	//origin: true
-	origin: "http://localhost:4200"
+	origin: "https://ce-vacation.web.app/"
 }));
 // Parse Request of Content-Type — application/json
 app.use(express.json());
